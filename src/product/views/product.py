6283d7de487fa71b0c879ed15e30103b84b782c8
RashiqAzhan <rashiqazhan@gmail.com>
2021-11-27 20:58:06 -0600

    Complete Data Filter.

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.views import generic
-from product.models import ProductVariantPrice, Variant
+from product.models import ProductVariant, ProductVariantPrice, Variant
 
 
 class IndexView(generic.ListView):
@@ -11,8 +12,41 @@ class IndexView(generic.ListView):
     page_range = paginator.get_elided_page_range(number=4)
 
     def get_queryset(self, **kwargs):
+        title_query = self.request.GET.get('title')
+        variant_query = self.request.GET.get('variant')
+        price_from_query = self.request.GET.get('price_from')
+        price_to_query = self.request.GET.get('price_to')
+        date_query = self.request.GET.get('date')
         query = ProductVariantPrice.objects.all()
+        if title_query:
+            query = query.filter(product__title__icontains=title_query)
+        elif variant_query:
+            query = query.filter(Q(product_variant_one__variant_title=variant_query)
+                                 | Q(product_variant_two__variant_title=variant_query)
+                                 | Q(product_variant_three__variant_title=variant_query))
+        elif price_from_query:
+            query = query.filter(price__gte=price_from_query)
+        elif price_to_query:
+            query = query.filter(price__lte=price_to_query)
+        elif date_query:
+            # date not defined in SQL file as shown in diagram
+            pass
         return query
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        filter_query = self.request.GET.get('title')
+        if filter_query:
+            color_query = ProductVariant.objects.filter(product__title__icontains=filter_query).filter(
+                    variant__title__icontains='Color').values_list('variant_title', flat=True).distinct()
+            size_query = ProductVariant.objects.filter(product__title__icontains=filter_query).filter(
+                    variant__title__icontains='Size').values_list('variant_title', flat=True).distinct()
+            style_query = ProductVariant.objects.filter(product__title__icontains=filter_query).filter(
+                    variant__title__icontains='Style').values_list('variant_title', flat=True).distinct()
+            context['color_variants'] = color_query
+            context['size_variants'] = size_query
+            context['style_variants'] = style_query
+        return context
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
